9d2fca502d511494fdb4cf756b71ff7f6a177c4a
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-10 14:08:47 +0200

    Update piholestats.py

--- piholestats.py
+++ piholestats.py
@@ -9,9 +9,11 @@ key = key.key
 data1 = requests.get(url="http://localhost/admin/api.php")
 jsonobj1 = json.loads(data1.content.decode('utf-8'))
 queries = jsonobj1["dns_queries_all_types"]
+unique_domains = jsonobj1["unique_domains"]
 print (queries)
+print (unique_domains)
 
-params = urllib.parse.urlencode({'field1': queries, 'key':key })
+params = urllib.parse.urlencode({'field1': queries, 'field2': unique_domains, 'key':key })
 headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
 conn = http.client.HTTPConnection("api.thingspeak.com:80")
 conn.request("POST", "/update", params, headers)
