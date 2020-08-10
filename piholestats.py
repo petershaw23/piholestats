3dbf83bede671a0e7356a164fe7d758cfcd67fb1
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-10 14:32:45 +0200

    Update piholestats.py

--- piholestats.py
+++ piholestats.py
@@ -8,18 +8,18 @@ key = key.key
 
 data1 = requests.get(url="http://localhost/admin/api.php")
 jsonobj1 = json.loads(data1.content.decode('utf-8'))
-queries = jsonobj1["dns_queries_all_types"]
+dns_queries_today = jsonobj1["dns_queries_today"]
 unique_domains = jsonobj1["unique_domains"]
 ads_blocked_today = jsonobj1["ads_blocked_today"]
 ads_percentage_today = jsonobj1["ads_percentage_today"]
 
-print ('queries:              '  +str(queries))
+print ('queries today:              '  +str(dns_queries_today))
 print ('unique_domains:       '  +str(unique_domains))
 print ('ads_blocked_today:    '  +str(ads_blocked_today))
 print ('ads_percentage_today: '  +str(ads_percentage_today))
 
 
-params = urllib.parse.urlencode({'field1': queries, 'field2': unique_domains, 'field3': ads_blocked_today, 'field4': ads_percentage_today, 'key':key })
+params = urllib.parse.urlencode({'field1': dns_queries_today, 'field2': unique_domains, 'field3': ads_blocked_today, 'field4': ads_percentage_today, 'key':key })
 headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
 conn = http.client.HTTPConnection("api.thingspeak.com:80")
 conn.request("POST", "/update", params, headers)
