c4d21b8a10fb29a1165f91717bcfdfcc7f15bcc4
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-10 13:33:51 +0200

    Update piholestats.py

--- piholestats.py
+++ piholestats.py
@@ -1,18 +1,8 @@
+import datetime
 import json
-import urllib2
- 
-try:
-    f = urllib2.urlopen('http://localhost/admin/api.php')
-    json_string = f.read()
-    parsed_json = json.loads(json_string)
-    queries = parsed_json['dns_queries_today']
-    adsblocked = parsed_json['ads_blocked_today']
-    clients = parsed_json['unique_clients']
-    f.close()
-except:
-    queries = '-'
-    adsblocked = '-'
-    clients = '-'
- 
-pihole = 'DNS-Queries: ' + str(queries) + ' - ' + 'Ads blocked: ' + str(adsblocked) + ' - ' + 'Devices: ' + str(clients)
-print (pihole)
+import requests
+import http.client, urllib.parse
+data1 = requests.get(url="http://localhost/admin/api.php")
+jsonobj1 = json.loads(data1.content.decode('utf-8'))
+queries = jsonobj1["dns_queries_all_types"]
+print (queries)
