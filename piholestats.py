76d6b2da485f61f74e934e77b809e4f9993ac63f
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-08-10 14:19:40 +0200

    Update piholestats.py

--- piholestats.py
+++ piholestats.py
@@ -11,12 +11,13 @@ jsonobj1 = json.loads(data1.content.decode('utf-8'))
 queries = jsonobj1["dns_queries_all_types"]
 unique_domains = jsonobj1["unique_domains"]
 ads_blocked_today = jsonobj1["ads_blocked_today"]
+ads_percentage_today = jsonobj1["ads_percentage_today"]
 
 print (queries)
 print (unique_domains)
 print (ads_blocked_today)
 
-params = urllib.parse.urlencode({'field1': queries, 'field2': unique_domains, 'field3': ads_blocked_today, 'key':key })
+params = urllib.parse.urlencode({'field1': queries, 'field2': unique_domains, 'field3': ads_blocked_today, 'field4': ads_percentage_today, 'key':key })
 headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
 conn = http.client.HTTPConnection("api.thingspeak.com:80")
 conn.request("POST", "/update", params, headers)
